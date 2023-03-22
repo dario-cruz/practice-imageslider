baf213b63e3a33fc1bdb8360fc879829ea1ac17e
Dario Cruz <rio2012@gmail.com>
2023-03-22 13:54:28 -0400

    Change js funcs increment and de-increment now work, added src prop to imgs.

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 // Define all of the elements to be used.
-const spanButtonLeft = document.querySelector('.right-arrow')
-const spanButtonRight = document.querySelector('.left-arrow')
+const spanButtonLeft = document.getElementById('left-arrow')
+const spanButtonRight = document.getElementById('right-arrow')
 console.log(spanButtonLeft, spanButtonRight)
 
 let counterValue = 1
 // Events for arrow buttons.
-spanButtonLeft.addEventListener('click', leftArrowCycle())
-spanButtonRight.addEventListener('click', rightArrowCycle())
+spanButtonLeft.addEventListener('click', leftArrowCycle)
+spanButtonRight.addEventListener('click', rightArrowCycle)
 
 
 // Function for cycling through elems. 
 function leftArrowCycle() {
     window.open(`#image-${counterValue}`, '_self')
-    counterValue = counterValue++
+    if (counterValue == 10) {
+        counterValue = 1
+    } else {
+        counterValue = ++counterValue
+    }
     console.log(counterValue)
 }
 function rightArrowCycle() {
     window.open(`#image-${counterValue}`, '_self')
-    counterValue = counterValue--
+    if (counterValue == 1) {
+        counterValue = 10
+    } else {
+        counterValue = --counterValue
+    }
     console.log(counterValue)  
 }
